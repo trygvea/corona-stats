a6303e0aeb0c45ba6671ad1d8542732e2df323ba
trygvea <trygve.amundsen@gmail.com>
2020-03-31 17:13:12 +0200

    Updates

---
+++ precache-manifest.74393941e2c2d85c3e5099d8613c2211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97911c3e8133c368eda140be45463e1",
+    "url": "/corona-stats/index.html"
+  },
+  {
+    "revision": "8ccfa2c25b1c8215f5f1",
+    "url": "/corona-stats/static/css/main.f9d6bc54.chunk.css"
+  },
+  {
+    "revision": "293847138cbbc73d4f3a",
+    "url": "/corona-stats/static/js/2.d9e1d2ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corona-stats/static/js/2.d9e1d2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ccfa2c25b1c8215f5f1",
+    "url": "/corona-stats/static/js/main.7d8404db.chunk.js"
+  },
+  {
+    "revision": "f4c5a3e34ad8a75407da",
+    "url": "/corona-stats/static/js/runtime-main.83884304.js"
+  }
+]);
